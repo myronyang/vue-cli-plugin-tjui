1303939c8f6be876d2344ada034eb59406c9f795
myronyang <821253835@qq.com>
2019-12-18 19:18:23 +0800

    update

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -30,7 +30,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 @import "@/assets/style/variable.scss";
 @import "@/assets/style/mixin.scss";
 
