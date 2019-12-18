dc9cd06fca6e547d883ae38b1b4c790348d7013c
myronyang <821253835@qq.com>
2019-12-18 11:11:24 +0800

    add

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const HelloWorld = r => require.ensure([], () => r(require('@/components/HelloWorld')), 'HelloWorld')
 
 Vue.use(Router)
 
