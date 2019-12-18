387d6b13ca5bf37e110434f736dfe102e9c48c93
myronyang <821253835@qq.com>
2019-12-18 10:58:16 +0800

    add

--- meta.js
+++ meta.js
@@ -180,7 +180,7 @@ module.exports = {
     'test/unit/setup.js': "unit && runner === 'jest'",
     'test/e2e/**/*': 'e2e',
     'src/router/**/*': 'router',
-    'src/store/**/*': 'vuex',
+    'src/store/index.js': 'vuex',
   },
   complete: function(data, { chalk }) {
     const green = chalk.green
